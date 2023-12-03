23cf9d69641113c00654ec06319346acf5fef10d
bigacz <sbigacz@gmail.com>
2023-12-03 21:10:24 +0100

    workspace integration

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import 'normalize.css';
 import './styles/general.css';
+import './styles/current.css';
 import './styles/days.css';
 import './styles/hours.css';
-import './styles/current.css';
 import WeatherBarrel from './scripts/weatherBarrel';
